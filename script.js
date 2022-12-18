c989a9395407306d4d7078796a29e7824ee5649d
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-12-18 22:19:08 +0200

    finishing select plan section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let formStatus="personalInfo";
 
 const steps=document.querySelectorAll(".step");
+const mainSteps=document.querySelectorAll(".main-step");
 
 // Controll Moving between Steps -- START
 
@@ -13,12 +14,15 @@ function toggleSteps(direction){
     if(!check)
         return 0;
     const active_step=document.querySelector(".step.active");
+    const active_main_step=document.querySelector(".main-step.active");
     const active_index=[...steps].indexOf(active_step);
     if(direction==="back" && active_index===0)
         return 0;
     const step=direction==="back"?-1:1;
     active_step.classList.toggle("active");
+    active_main_step.classList.toggle("active");
     steps[active_index+step].classList.toggle("active");
+    mainSteps[active_index+step].classList.toggle("active");
     if((step+active_index)===3){
         nextBtn.classList.toggle("hidden");
         confirmBtn.classList.toggle("hidden");
@@ -55,7 +59,6 @@ function validatePersonalInfo(){
         if(data[0]!=="email" && !data[1]){
             inputFields[index].classList.add("error");
             status=false;
-            console.log(data[0]!=="email")
         }
         if(data[0]==="email"){
             const regex=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
@@ -64,8 +67,31 @@ function validatePersonalInfo(){
                 status=false;
             };
         }
-        return status;
     })
+    return status;
 }
 
 // Personal Info Validation -- END
+
+// Selecting Plan & Toggle billing -- START
+
+const billingToggle=document.querySelector(".billing-toggle input");
+const monthly=document.querySelector("label.monthly")
+const yearly=document.querySelector("label.yearly");
+const planPrices=document.querySelectorAll(".plan-price");
+let billing="monthly";
+const monthlyBilling=[9,12,15];
+const yearlyBilling=[90,120,150];
+
+billingToggle.addEventListener("change",()=>{
+    monthly.classList.toggle("active");
+    yearly.classList.toggle("active");
+    billing=billing==="monthly"?"yearly":"monthly";
+    planPrices.forEach((planPrice,index)=>{
+        const price=billing==="monthly"?`$${monthlyBilling[index]}/mo`:`$${yearlyBilling[index]}/yr`;
+        planPrice.textContent=price;
+        billing==="monthly"?planPrice.classList.remove("yearly"):planPrice.classList.add("yearly");
+    })
+});
+
+// Selecting Plan & Toggle billing -- END
