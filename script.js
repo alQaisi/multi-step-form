ae8c86d3c3d6eb44afd22ba60854dfac7f5a6ff3
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-12-19 18:08:43 +0200

    finishing step2 & step3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let formStatus="personalInfo";
+let billing="monthly";
+let total=0;
 
 const steps=document.querySelectorAll(".step");
 const mainSteps=document.querySelectorAll(".main-step");
@@ -30,6 +32,7 @@ function toggleSteps(direction){
         nextBtn.classList.toggle("hidden");
         confirmBtn.classList.toggle("hidden");
     }
+    total=calculateTotal();
 }
 
 // Controll Moving between Steps -- END
@@ -73,15 +76,37 @@ function validatePersonalInfo(){
 
 // Personal Info Validation -- END
 
+// Picking add-ons -- START
+
+const monthlyAddOns=[1,2,2];
+const yearlyAddOns=[10,20,20];
+
+const addOns=[...document.querySelectorAll(".add-on input")];
+const addOnsPrices=[...document.querySelectorAll(".add-on-price")];
+
+addOns.forEach(addOn=>addOn.addEventListener("change",calculateTotal));
+
+// Picking add-ons -- END
+
 // Selecting Plan & Toggle billing -- START
 
+const plans=[...document.querySelectorAll(".plan")];
+let activePlan=document.querySelector(".plan.active");
 const billingToggle=document.querySelector(".billing-toggle input");
 const monthly=document.querySelector("label.monthly")
 const yearly=document.querySelector("label.yearly");
 const planPrices=document.querySelectorAll(".plan-price");
-let billing="monthly";
 const monthlyBilling=[9,12,15];
 const yearlyBilling=[90,120,150];
+
+plans.forEach(plan=>{
+    plan.addEventListener("click",function(){
+        activePlan.classList.remove("active");
+        this.classList.add("active");
+        activePlan=this;
+        total=calculateTotal();
+    })
+})
 
 billingToggle.addEventListener("change",()=>{
     monthly.classList.toggle("active");
@@ -91,7 +116,24 @@ billingToggle.addEventListener("change",()=>{
         const price=billing==="monthly"?`$${monthlyBilling[index]}/mo`:`$${yearlyBilling[index]}/yr`;
         planPrice.textContent=price;
         billing==="monthly"?planPrice.classList.remove("yearly"):planPrice.classList.add("yearly");
-    })
+    });
+    addOnsPrices.forEach((addOnPrice,index)=>{
+        const price=billing==="monthly"?`$${monthlyAddOns[index]}/mo`:`$${yearlyAddOns[index]}/yr`;
+        addOnPrice.textContent=price;
+    });
+    total=calculateTotal();
 });
 
 // Selecting Plan & Toggle billing -- END
+
+function calculateTotal(){
+    const planPrice=billing==="monthly"?monthlyBilling[plans.indexOf(activePlan)]:yearlyBilling[plans.indexOf(activePlan)];
+    const addOnsPrice=addOns.reduce(function(acc,addOn){
+        const price=billing==="monthly"?monthlyAddOns[addOns.indexOf(addOn)]:yearlyAddOns[addOns.indexOf(addOn)];
+        if(addOn.checked)
+            return acc+price;
+        return acc;
+    },0)
+    console.log(planPrice+addOnsPrice);
+    return planPrice+addOnsPrice;
+}
